4947739367c6dc806ed7e932c5a01cc8d5fc3ddf
David <david@cadeyrn.us>
2014-09-29 01:08:04 -0500

    Added techno-globals.lua with areas.Assault table definition.

---
+++ gear/techno-globals.lua
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------
+-- This is for personal globals, as opposed to library globals.
+--------------------------------------------------------------------------------
+
+-- Aht Urghan areas that enable Assault bonuses on gear.
+areas.Assault = S{
+  "Ilrusi Atoll",
+  "Mamool Ja Training Grounds",
+  "Periqia",
+  "Lebros Cavern",
+  "Leujaoam Sanctum",
+  "Nyzul Isle",
+  "The Ashu Talif",
+  "Arrapago Remnants",
+  "Silver Sea Remnants",
+  "Zhayolm Remnants",
+  "Bhaflau Remnants",
+}
