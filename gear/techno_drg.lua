189cc7c98d38fd3dd219e0c79a77fedb6fd14464
David <david@cadeyrn.us>
2014-09-29 01:58:53 -0500

    Added Assault combat form and sets.

--- gear/techno_drg.lua
+++ gear/techno_drg.lua
@@ -84,6 +84,14 @@ function init_gear_sets()
     waist = "Wyrm Belt",
   })
 
+  -- Melee sets for use in Assault and Salvage.  Sets should utilize gear with
+  -- Assault bonuses.
+  sets.engaged.Assault = set_combine(sets.engaged, {
+    ring1 = "Imperial Ring",
+  })
+
+  sets.engaged.Assault.Acc = set_combine(sets.engaged.Acc, {})
+
 
   -- Precast Sets
   -- Precast sets to enhance JAs
@@ -213,7 +221,11 @@ end
 -------------------------------------------------------------------------------------------------------------------
 
 function update_combat_form()
-
+  if areas.Assault:contains(world.area) then
+    state.CombatForm:set('Assault')
+  else
+    state.CombatForm:reset()
+  end
 end
 
 -- Select default macro book on initial load or subjob change.
