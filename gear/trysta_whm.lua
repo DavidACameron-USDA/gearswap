b13c9ec339db03a065976a8a10cce2d54efed1f7
David <david@cadeyrn.us>
2015-01-25 18:13:53 -0600

    Set idling weapon to Eminent Wand in trysta_whm.

--- gear/trysta_whm.lua
+++ gear/trysta_whm.lua
@@ -227,7 +227,7 @@ function init_gear_sets()
 
   -- Idle sets (default idle set not needed since the other three are defined, but leaving for testing purposes)
   sets.idle = {
-    main = "Tamaxchi",
+    main = "Eminent Wand",
     sub = "Sors Shield",
     ammo = "Kalboron Stone",
     head = "Wayfarer Circlet",
