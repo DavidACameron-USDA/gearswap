0ca886e32445b8f5fdd9efe6ef5e641e19dfe6d1
David Cameron <david@cadeyrn.us>
2017-02-19 21:47:07 -0600

    Added techno_run.

---
+++ gear/techno_run.lua
@@ -0,0 +1,290 @@
+--------------------------------------------------------------------------------
+-- User setup functions for this job.
+--------------------------------------------------------------------------------
+
+function user_setup()
+  state.OffenseMode:options('Normal', 'DD', 'Acc', 'PDT', 'MDT')
+  state.WeaponskillMode:options('Normal', 'Acc')
+  state.PhysicalDefenseMode:options('PDT')
+  state.IdleMode:options('Regen', 'Refresh')
+
+	select_default_macro_book()
+end
+
+
+function init_gear_sets()
+  sets.enmity = {
+    neck = "Warder's Charm",
+    ring2 = "Petrov Ring",
+    back = "Evasionist's Cape",
+    waist = "Warwolf Belt",
+  }
+
+	--------------------------------------
+	-- Precast sets
+	--------------------------------------
+
+	-- Precast sets to enhance JAs
+  sets.precast.JA['Vallation'] = {}
+
+  sets.precast.JA['Valiance'] = sets.precast.JA['Vallation']
+
+  sets.precast.JA['Pflug'] = {}
+
+  sets.precast.JA['Battuta'] = {}
+
+  sets.precast.JA['Liement'] = {}
+
+  sets.precast.JA['Lunge'] = {
+    neck = "Baetyl Pendant",
+    ear1 = "Friomisi Earring",
+    ear2 = "Hecate's Earring",
+    body = "Rawhide Vest",
+    back = "Argochampsa Mantle",
+    waist = "Yamabuki-no-Obi",
+    feet = "Herculean Boots",
+  }
+
+  sets.precast.JA['Swipe'] = sets.precast.JA['Lunge']
+
+  sets.precast.JA['Gambit'] = {}
+
+  sets.precast.JA['Rayke'] = {}
+
+  sets.precast.JA['Elemental Sforzo'] = {}
+
+  sets.precast.JA['Swordplay'] = {}
+
+  sets.precast.JA['Embolden'] = {
+    back = "Evasionist's Cape",
+  }
+
+  -- Vivacious Pulse is enhanced by Divine Magic skill.
+  sets.precast.JA['Vivacious Pulse'] = {
+    neck = "Incanter's Torque",
+  }
+
+  sets.precast.JA['One For All'] = {}
+
+  sets.precast.JA['Provoke'] = sets.enmity
+
+
+	-- Fast cast sets for spells
+  sets.precast.FC = {
+    ear2 = "Loquacious Earring",
+    neck = "Baetyl Pendant",
+    ring1 = "Weatherspoon Ring",
+    legs = "Rawhide Trousers",
+  }
+
+  sets.precast.FC['Enhancing Magic'] = set_combine(sets.precast.FC, {
+    waist = "Siegel Sash",
+  })
+
+  sets.precast.FC['Utsusemi: Ichi'] = set_combine(sets.precast.FC, {
+    neck = "Magoraga Beads",
+  })
+
+  sets.precast.FC['Utsusemi: Ni'] = sets.precast.FC['Utsusemi: Ichi']
+
+
+	-- Weaponskill sets
+
+  -- Resolution has a STR 73~85% modifier and is aligned with the Breeze,
+  -- Thunder, and Soil gorgets/belts.
+  sets.precast.WS['Resolution'] = {
+    ammo = "Aqreqaq Bomblet",
+    head = "Adhemar Bonnet",
+    neck = gear.ElementalGorget,
+    ear1 = "Brutal Earring",
+    ear2 = "Moonshade Earring",
+    body = "Rawhide Vest",
+    hands = "Herculean Gloves",
+    ring1 = "Epona's Ring",
+    ring2 = "Ifrit's Ring",
+    back = "Atheling Mantle",
+    waist = gear.ElementalBelt,
+    legs = "Taeon Tights",
+    feet = "Herculean Boots",
+  }
+
+  sets.precast.WS['Resolution'].Acc = set_combine(sets.precast.WS['Resolution'].Normal, {
+    ammo = "Honed Tathlum",
+    head = "Ayanmo Zucchetto",
+    body = "Ayanmo Corazza",
+    hands = "Ayanmo Manopolas +1",
+    ring1 = "Rufescent Ring",
+    waist = "Kentarch Belt",
+    legs = "Ayanmo Cosciales",
+    feet = "Ayanmo Gambieras +1",
+  })
+
+  -- Dimidation has a DEX 80% modifier and is aligned with the Thunder and
+  -- Breeze gorgets/belts.
+  sets.precast.WS['Dimidiation'] = {
+    ammo = "Jukukik Feather",
+    head = "Adhemar Bonnet",
+    neck = gear.ElementalGorget,
+    ear1 = "Brutal Earring",
+    ear2 = "Moonshade Earring",
+    body = "Rawhide Vest",
+    hands = "Herculean Gloves",
+    ring1 = "Epona's Ring",
+    ring2 = "Ramuh Ring",
+    back = "Atheling Mantle",
+    waist = gear.ElementalBelt,
+    legs = "Taeon Tights",
+    feet = "Herculean Boots",
+  }
+
+  sets.precast.WS['Dimidiation'].Acc = set_combine(sets.precast.WS['Dimidiation'].Normal, {
+    ammo = "Honed Tathlum",
+    head = "Ayanmo Zucchetto",
+    body = "Ayanmo Corazza",
+    hands = "Ayanmo Manopolas +1",
+    ring1 = "Rufescent Ring",
+    waist = "Kentarch Belt",
+    legs = "Ayanmo Cosciales",
+    feet = "Ayanmo Gambieras +1",
+  })
+
+  -- Herculean Slash has a VIT 80% modifier, is magical and therefore modified
+  -- by MAB, and is aligned with the Thunder, Breeze, and Snow gorgets/belts.
+  sets.precast.WS['Herculean Slash'] = set_combine(sets.precast['Lunge'], {})
+
+
+	--------------------------------------
+	-- Midcast sets
+	--------------------------------------
+
+  sets.midcast.FastRecast = {}
+
+  sets.midcast['Enhancing Magic'] = {
+    neck = "Incanter's Torque",
+    legs = "Carmine Cuisses",
+  }
+
+  sets.midcast['Phalanx'] = set_combine(sets.midcast['Enhancing Magic'], {})
+
+  sets.midcast['Regen'] = {}
+
+  sets.midcast['Stoneskin'] = {
+    waist = "Siegel Sash",
+  }
+
+  sets.midcast.Cure = {
+    neck = "Incanter's Torque",
+    back = "Solemnity Cape",
+    legs = "Carmine Cuisses",
+  }
+
+  sets.midcast.Flash = sets.enmity
+
+	--------------------------------------
+	-- Idle/resting/defense/etc sets
+	--------------------------------------
+
+  sets.idle = {
+    main = "Montante",
+    sub = "Refined Grip +1",
+    ammo = "Ginsen",
+    head = "Ayanmo Zucchetto",
+    neck = "Twilight Torque",
+    ear1 = "Steelflash Earring",
+    ear2 = "Bladeborn Earring",
+    body = "Ayanmo Corazza",
+    hands = "Ayanmo Manopolas +1",
+    ring1 = "Defending Ring",
+    ring2 = "Petrov Ring",
+    back = "Solemnity Cape",
+    waist = "Flume Belt +1",
+    legs = "Carmine Cuisses",
+    feet = "Ayanmo Gambieras +1",
+  }
+
+  sets.idle.Refresh = set_combine(sets.idle, {
+    legs = "Rawhide Trousers",
+  })
+
+	sets.defense.PDT = {}
+
+	sets.defense.MDT = {}
+
+	sets.Kiting = {}
+
+
+	--------------------------------------
+	-- Engaged sets
+	--------------------------------------
+
+  sets.engaged = {
+  }
+
+  sets.engaged.DD = {
+    sub = "Bloodrain Strap",
+    ammo = "Ginsen",
+    head = "Adhemar Bonnet",
+    neck = "Asperity Necklace",
+    ear1 = "Steelflash Earring",
+    ear2 = "Bladeborn Earring",
+    body = "Rawhide Vest",
+    hands = "Herculean Gloves",
+    ring1 = "Epona's Ring",
+    ring2 = "Petrov Ring",
+    back = "Atheling Mantle",
+    waist = "Windbuffet Belt +1",
+    legs = "Taeon Tights",
+    feet = "Herculean Boots",
+  }
+
+  sets.engaged.Acc = set_combine(sets.engaged.DD, {
+    head = "Ayanmo Zucchetto",
+    neck = "Sanctity Necklace",
+    body = "Ayanmo Corazza",
+    hands = "Ayanmo Manopolas +1",
+    waist = "Kentarch Belt",
+    legs = "Ayanmo Cosciales",
+    feet = "Ayanmo Gambieras +1",
+  })
+
+  sets.engaged.PDT = {
+    sub = "Refined Grip +1",
+    head = "Ayanmo Zucchetto",
+    neck = "Twilight Torque",
+    body = "Ayanmo Corazza",
+    hands = "Ayanmo Manopolas +1",
+    ring1 = "Defending Ring",
+    back = "Evasionist's Cape",
+    waist = "Flume Belt +1",
+    feet = "Ayanmo Gambieras +1",
+  }
+
+  sets.engaged.MDT = {
+    sub = "Refined Grip +1",
+    head = "Ayanmo Zucchetto",
+    neck = "Twilight Torque",
+    body = "Ayanmo Corazza",
+    hands = "Ayanmo Manopolas +1",
+    ring1 = "Defending Ring",
+    back = "Solemnity Cape",
+    waist = "Flume Belt +1",
+    feet = "Ayanmo Gambieras +1",
+  }
+
+end
+
+
+-------------------------------------------------------------------------------------------------------------------
+-- Utility functions specific to this job.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Select default macro book on initial load or subjob change.
+function select_default_macro_book()
+	-- Default macro set/book
+	if player.sub_job == 'WAR' then
+		set_macro_page(10, 20)
+	else
+		set_macro_page(10, 20)
+	end
+end
+
---
+++ run.lua
@@ -0,0 +1,218 @@
+--------------------------------------------------------------------------------
+-- Setup functions for this job.  Generally should not be modified.
+--------------------------------------------------------------------------------
+
+-- Initialization function for this job file.
+function get_sets()
+  mote_include_version = 2
+
+	-- Load and initialize the include file.
+	include('Mote-Include.lua')
+end
+
+
+-- Setup vars that are user-independent.
+function job_setup()
+  -- Table of entries
+  rune_timers = T{}
+  -- entry = rune, index, expires
+
+  if player.main_job_level >= 65 then
+    max_runes = 3
+  elseif player.main_job_level >= 35 then
+    max_runes = 2
+  elseif player.main_job_level >= 5 then
+    max_runes = 1
+  else
+    max_runes = 0
+  end
+end
+
+------------------------------------------------------------------
+-- Action events
+------------------------------------------------------------------
+
+-- Run after the default midcast() is done.
+-- eventArgs is the same one used in job_midcast, in case information needs to be persisted.
+function job_post_midcast(spell, action, spellMap, eventArgs)
+  if spell.english == 'Lunge' or spell.english == 'Swipe' then
+    local obi = get_obi(get_rune_obi_element())
+    if obi then
+      equip({waist=obi})
+    end
+  end
+end
+
+
+function job_aftercast(spell)
+  if not spell.interrupted then
+    if spell.type == 'Rune' then
+      update_timers(spell)
+    elseif spell.name == "Lunge" or spell.name == "Gambit" or spell.name == 'Rayke' then
+      reset_timers()
+    elseif spell.name == "Swipe" then
+      send_command(trim(1))
+    end
+  end
+end
+
+
+-------------------------------------------------------------------------------------------------------------------
+-- Customization hooks for idle and melee sets, after they've been automatically constructed.
+-------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------
+-- General hooks for other events.
+-------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------
+-- User code that supplements self-commands.
+-------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------
+-- Utility functions specific to this job.
+-------------------------------------------------------------------------------------------------------------------
+
+function get_rune_obi_element()
+  weather_rune = buffactive[elements.rune_of[world.weather_element] or '']
+  day_rune = buffactive[elements.rune_of[world.day_element] or '']
+
+  local found_rune_element
+
+  if weather_rune and day_rune then
+    if weather_rune > day_rune then
+      found_rune_element = world.weather_element
+    else
+      found_rune_element = world.day_element
+    end
+  elseif weather_rune then
+    found_rune_element = world.weather_element
+  elseif day_rune then
+    found_rune_element = world.day_element
+  end
+
+  return found_rune_element
+end
+
+function get_obi(element)
+  if element and elements.obi_of[element] then
+    return (player.inventory[elements.obi_of[element]] or player.wardrobe[elements.obi_of[element]]) and elements.obi_of[element]
+  end
+end
+
+
+------------------------------------------------------------------
+-- Timer manipulation
+------------------------------------------------------------------
+
+-- Add a new run to the custom timers, and update index values for existing timers.
+function update_timers(spell)
+  local expires_time = os.time() + 300
+  local entry_index = rune_count(spell.name) + 1
+
+  local entry = {rune=spell.name, index=entry_index, expires=expires_time}
+
+  rune_timers:append(entry)
+  local cmd_queue = create_timer(entry).. ';wait 0.05;'
+
+  cmd_queue = cmd_queue .. trim()
+
+  add_to_chat(123,'cmd_queue='..cmd_queue)
+
+  send_command(cmd_queue)
+end
+
+-- Get the command string to create a custom timer for the provided entry.
+function create_timer(entry)
+  local timer_name = '"Rune: ' .. entry.rune .. '-' .. tostring(entry.index) .. '"'
+  local duration = entry.expires - os.time()
+  return 'timers c ' .. timer_name .. ' ' .. tostring(duration) .. ' down'
+end
+
+-- Get the command string to delete a custom timer for the provided entry.
+function delete_timer(entry)
+  local timer_name = '"Rune: ' .. entry.rune .. '-' .. tostring(entry.index) .. '"'
+  return 'timers d ' .. timer_name .. ''
+end
+
+-- Reset all timers
+function reset_timers()
+  local cmd_queue = ''
+  for index,entry in pairs(rune_timers) do
+      cmd_queue = cmd_queue .. delete_timer(entry) .. ';wait 0.05;'
+  end
+  rune_timers:clear()
+  send_command(cmd_queue)
+end
+
+-- Get a count of the number of runes of a given type
+function rune_count(rune)
+  local count = 0
+  local current_time = os.time()
+  for _,entry in pairs(rune_timers) do
+    if entry.rune == rune and entry.expires > current_time then
+      count = count + 1
+    end
+  end
+  return count
+end
+
+-- Remove the oldest rune(s) from the table, until we're below the max_runes limit.
+-- If given a value n, remove n runes from the table.
+function trim(n)
+  local cmd_queue = ''
+
+  local to_remove = n or (rune_timers:length() - max_runes)
+
+  while to_remove > 0 and rune_timers:length() > 0 do
+    local oldest
+    for index,entry in pairs(rune_timers) do
+      if oldest == nil or entry.expires < rune_timers[oldest].expires then
+        oldest = index
+      end
+    end
+
+    cmd_queue = cmd_queue .. prune(rune_timers[oldest].rune)
+    to_remove = to_remove - 1
+  end
+
+  return cmd_queue
+end
+
+-- Drop the index of all runes of a given type.
+-- If the index drops to 0, it is removed from the table.
+function prune(rune)
+  local cmd_queue = ''
+
+  for index,entry in pairs(rune_timers) do
+    if entry.rune == rune then
+      cmd_queue = cmd_queue .. delete_timer(entry) .. ';wait 0.05;'
+      entry.index = entry.index - 1
+    end
+  end
+
+  for index,entry in pairs(rune_timers) do
+    if entry.rune == rune then
+      if entry.index == 0 then
+        rune_timers[index] = nil
+      else
+        cmd_queue = cmd_queue .. create_timer(entry) .. ';wait 0.05;'
+      end
+    end
+  end
+
+  return cmd_queue
+end
+
+
+------------------------------------------------------------------
+-- Reset events
+------------------------------------------------------------------
+
+windower.raw_register_event('zone change',reset_timers)
+windower.raw_register_event('logout',reset_timers)
+windower.raw_register_event('status change',function(new, old)
+  if gearswap.res.statuses[new].english == 'Dead' then
+    reset_timers()
+  end
+end)
